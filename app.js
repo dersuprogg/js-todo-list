a09ed9b7bf899c0bd181f4cb02e20bc880594731
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-03-12 18:16:53 +0500

    Refactor add todo functionality

--- app.js
+++ app.js
@@ -9,28 +9,33 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+// Main functions of the app
+function addTodoToDOM(todo) {
+  // Create todo elements
+  const li = document.createElement("li");
+  const p = document.createElement("p");
+  const btn = document.createElement("button");
+  // Create contents fo todo
+  const todoText = document.createTextNode(capitalize(todo));
+  p.append(todoText);
+  p.classList.add("todo__item");
+  li.append(p);
+  // Create remove btn contents
+  btn.innerHTML = "&times";
+  btn.classList.add("btn", "btn--remove");
+  li.append(btn);
+
+  li.classList.add("todo");
+
+  todoList.append(li);
+
+  formInput.value = "";
+}
+
 // Add event listeners
 addTodoBtn.addEventListener("click", (e) => {
   if (formInput.value.trim() !== "") {
-    // Create todo elements
-    const li = document.createElement("li");
-    const p = document.createElement("p");
-    const btn = document.createElement("button");
-    // Create contents fo todo
-    const todoText = document.createTextNode(capitalize(formInput.value));
-    p.append(todoText);
-    p.classList.add("todo__item");
-    li.append(p);
-    // Create remove btn contents
-    btn.innerHTML = "&times";
-    btn.classList.add("btn", "btn--remove");
-    li.append(btn);
-
-    li.classList.add("todo");
-
-    todoList.append(li);
-
-    formInput.value = "";
+    addTodoToDOM(formInput.value);
   }
   e.preventDefault();
 });
